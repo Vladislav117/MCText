b20a7695a6a3cbd5d972f1c6f8fef4633c74b806
Vladislav117 <siluetkysta@gmail.com>
2025-06-30 15:05:07 +0300

    Upload 0.0.0

---
+++ src/main/java/ru/vladislav117/mctext/TextComponent.java
@@ -0,0 +1,73 @@
+package ru.vladislav117.mctext;
+
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.Nullable;
+import ru.vladislav117.mctext.style.StyleLike;
+
+/**
+ * Текст с компонентом.
+ */
+public class TextComponent extends Text {
+    protected Component component;
+
+    /**
+     * Создание текста с компонентом.
+     *
+     * @param component Компонент
+     * @param style     Стиль или null, чтобы сделать стиль незаданным
+     */
+    public TextComponent(Component component, @Nullable StyleLike style) {
+        this.component = component;
+        this.style = style;
+    }
+
+    /**
+     * Создание текста с компонентом.
+     *
+     * @param component Компонент
+     */
+    public TextComponent(Component component) {
+        this.component = component;
+    }
+
+    /**
+     * Копирование текста с компонентом. Компонент не будет скопирован.
+     *
+     * @param text Текст с компонентом
+     */
+    public TextComponent(TextComponent text) {
+        component = text.component;
+        if (text.style != null) style = text.style.clone();
+    }
+
+    @Override
+    public Text clone() {
+        return new TextComponent(this);
+    }
+
+    /**
+     * Получение компонента.
+     *
+     * @return Компонент.
+     */
+    public Component getComponent() {
+        return component;
+    }
+
+    /**
+     * Установка компонента.
+     *
+     * @param component Компонент
+     * @return Этот же текст.
+     */
+    public TextComponent setComponent(Component component) {
+        this.component = component;
+        return this;
+    }
+
+    @Override
+    public Component toComponent() {
+        if (style != null) return Component.empty().append(component).style(style.toStyle());
+        return component;
+    }
+}
---
+++ src/main/java/ru/vladislav117/mctext/TextLike.java
@@ -0,0 +1,141 @@
+package ru.vladislav117.mctext;
+
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.Nullable;
+import ru.vladislav117.mctext.style.StyleLike;
+
+import java.util.Collection;
+
+/**
+ * Интерфейс, экземпляры реализаций которого могут быть текстом.
+ */
+public interface TextLike extends Cloneable {
+    /**
+     * Получение стиля текста.
+     *
+     * @return Стиль текста или null, если стиль не задан.
+     */
+    default @Nullable StyleLike getStyle() {
+        return null;
+    }
+
+    /**
+     * Установка стиля текста.
+     *
+     * @param style Стиль текста или null, чтобы сделать стиль незаданным
+     * @return Этот же текст.
+     */
+    default TextLike setStyle(@Nullable StyleLike style) {
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение.
+     *
+     * @param audience Получатель или null
+     * @return Этот же текст.
+     */
+    default TextLike sendMessage(@Nullable Audience audience) {
+        if (audience != null) audience.sendMessage(toComponent());
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение.
+     *
+     * @param audiences Получатели или null
+     * @return Этот же текст.
+     */
+    default TextLike sendMessage(@Nullable Collection<@Nullable Audience> audiences) {
+        if (audiences == null) return this;
+        Component component = toComponent();
+        for (Audience audience : audiences) {
+            if (audience != null) audience.sendMessage(component);
+        }
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение.
+     *
+     * @param audiences Получатели или null
+     * @return Этот же текст.
+     */
+    default TextLike sendMessage(@Nullable Audience[] audiences) {
+        if (audiences == null) return this;
+        Component component = toComponent();
+        for (Audience audience : audiences) {
+            if (audience != null) audience.sendMessage(component);
+        }
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение для хотбара.
+     *
+     * @param audience Получатель или null
+     * @return Этот же текст.
+     */
+    default TextLike sendActionBar(@Nullable Audience audience) {
+        if (audience != null) audience.sendActionBar(toComponent());
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение для хотбара.
+     *
+     * @param audiences Получатели или null
+     * @return Этот же текст.
+     */
+    default TextLike sendActionBar(@Nullable Collection<@Nullable Audience> audiences) {
+        if (audiences == null) return this;
+        Component component = toComponent();
+        for (Audience audience : audiences) {
+            if (audience != null) audience.sendActionBar(component);
+        }
+        return this;
+    }
+
+    /**
+     * Отправить этот текст как сообщение для хотбара.
+     *
+     * @param audiences Получатели или null
+     * @return Этот же текст.
+     */
+    default TextLike sendActionBar(@Nullable Audience[] audiences) {
+        if (audiences == null) return this;
+        Component component = toComponent();
+        for (Audience audience : audiences) {
+            if (audience != null) audience.sendActionBar(component);
+        }
+        return this;
+    }
+
+    /**
+     * Создание контейнера и добавление этого текста внутрь.
+     *
+     * @return Созданный контейнер.
+     */
+    default TextContainer toContainer() {
+        return new TextContainer(this);
+    }
+
+    /**
+     * Создание контейнера и добавление копии этого текста внутрь.
+     *
+     * @return Созданный контейнер.
+     */
+    default TextContainer toContainerCopy() {
+        return new TextContainer(clone());
+    }
+
+    TextLike clone();
+
+    /**
+     * Преобразование этого текста к компоненту.
+     *
+     * @return Компонент.
+     */
+    Component toComponent();
+}
